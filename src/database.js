0dbe42c3168c223ff08d2944991b65ff1996aa69
Servicios Informáticos <jluis.aguirre@udea.edu.co>
2020-12-29 18:18:40 -0500

    Name database

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/quotes-db-app', {
+mongoose.connect('mongodb+srv://quotes-db-app:josefo1234@cluster0.zdi2k.mongodb.net/<dbname>?retryWrites=true&w=majority', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: true,
