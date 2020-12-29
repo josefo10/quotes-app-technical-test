3585e2043343d72b9f1203a29197772c64b58fc5
Servicios Informáticos <jluis.aguirre@udea.edu.co>
2020-12-29 14:34:25 -0500

    add error validation

--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ mongoose.connect('mongodb://localhost/quotes-db-app', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: true,
-    useUnifiedTopology: true 
+    useUnifiedTopology: true
 })
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err));
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,7 @@ const router = Router();
 const {imageapi, quoteapi} = require('../services/servicesQuote');
 const {saveDataQuote, quoteDelete, quoteFind} = require('../services/servicesDatabase');
 
-const quote = require('../models/quotes');
+//const quote = require('../models/quotes');
 
 
 router.get('/api/v1/generate-Changing-life-quote', async (req, res) =>{
@@ -41,7 +41,6 @@ router.delete('/api/v1/delete-Changing-life-quote/:id', async(req, res) => {
     try{
         const id = req.params.id;
         const deleteQuote = await quoteDelete(id);
-        //const data = await quote.deleteOne({_id:id});
         if(deleteQuote.deletedCount > 0){
             res.status(200).send('The quote was successfully deleted');
         }else{
